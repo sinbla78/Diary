ddfb021ffc374de35d7ab7c3954c1b82a9cd27f1
양희범 <sinbla78@gmail.com>
2024-03-19 10:01:16 +0900

    FIX: 프로젝트 세팅

--- diary/src/main/java/com/example/diary/global/error/handler/ErrorCode.java
+++ diary/src/main/java/com/example/diary/global/error/handler/ErrorCode.java
@@ -6,11 +6,19 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+
     USER_NOT_FOUND(404, "유저를 찾을 수 없습니다."),
-    NOT_MATCHES_PASSWORD(400, "비밀번호가 올바르지 않습니다."),
+    ADMIN_NOT_FOUND(404, "관리자를 찾을 수 없습니다."),
+    ISSUE_ALREADY_EXISTS(409, "이미 발급된 계정입니다."),
+    INFO_NOT_FOUND(404,"인적사항을 찾을 수 없습니다."),
+    INFO_ALREADY_EXISTS(409, "이미 존재하는 인적사항입니다."),
+    IS_EMPTY(400, "값이 비어있습니다."),
 
     EXPIRED_JWT(401, "토큰이 만료되었습니다."),
-    INVALID_JWT(401, "토큰이 유효하지 않습니다.");
+    INVALID_JWT(401, "토큰이 유효하지 않습니다."),
+    NOT_REFRESH_TOKEN(401,"refresh 토큰이 아닙니다."),
+    PASSWORD_MIS_MATCH(401, "비밀번호가 올바르지 않습니다."),
+    NOT_FOUND_REFRESH_TOKEN(404, "refresh 토큰을 찾을 수 없습니다.");
 
     private final int status;
     private final String message;
