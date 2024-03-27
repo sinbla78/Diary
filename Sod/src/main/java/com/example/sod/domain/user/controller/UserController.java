3ab1e10cba0c5dcaa0afeb844e319ab06313c3be
양희범 <ay612@dsm.hs.kr>
2024-03-27 19:38:56 +0900

    ADD: logout 구현

--- Sod/src/main/java/com/example/sod/domain/user/controller/UserController.java
+++ Sod/src/main/java/com/example/sod/domain/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.sod.domain.user.controller;
 
 import com.example.sod.domain.user.controller.dto.request.SignUpRequest;
 import com.example.sod.domain.user.controller.dto.request.UpdateInfoRequest;
+import com.example.sod.domain.user.service.LogoutService;
 import com.example.sod.domain.user.service.SignUpService;
 import com.example.sod.domain.user.service.UpdateInfoService;
 import com.example.sod.domain.user.service.WithdrawalService;
@@ -19,11 +20,18 @@ public class UserController {
     private final SignUpService signUpService;
     private final WithdrawalService withdrawalService;
     private final UpdateInfoService updateInfoService;
+    private final LogoutService logoutService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public void signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
         signUpService.execute(signUpRequest);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/logout")
+    public void logout() {
+        logoutService.execute();
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
